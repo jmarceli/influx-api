6754524415e1552f0dd7259c068c626f6ce319f1
Jan Grzegorowski <grzegorowski@gmail.com>
2018-09-28 09:02:18 +0200

    chore: use rollup-plugin-cpy

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,22 @@
 import babel from 'rollup-plugin-babel';
-import copy from 'rollup-plugin-copy';
+import copy from 'rollup-plugin-cpy';
 import flow from 'rollup-plugin-flow';
+
+const plugins = [
+  flow(),
+  babel({
+    exclude: 'node_modules/**',
+    babelrc: false,
+    presets: [['env', { modules: false }]],
+    plugins: [['transform-object-rest-spread', { useBuiltIns: true }]],
+  }),
+  copy({
+    files: ['src/*.flow'],
+    dest: 'lib',
+  }),
+];
+
+const external = ['axios', 'qs'];
 
 export default [{
   input: 'src/index.js',
@@ -10,19 +26,8 @@ export default [{
     format: 'esm',
     sourcemap: true,
   },
-  external: ['axios', 'qs'],
-  plugins: [
-    flow(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      presets: [['env', { modules: false }]],
-      plugins: [['transform-object-rest-spread', { useBuiltIns: true }]],
-    }),
-    copy({
-      'src/index.js.flow': 'lib/index.js.flow',
-    }),
-  ],
+  external,
+  plugins,
 }, {
   input: 'src/index.js',
   output: {
@@ -31,17 +36,6 @@ export default [{
     format: 'cjs',
     sourcemap: true,
   },
-  external: ['axios', 'qs'],
-  plugins: [
-    flow(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      presets: [['env', { modules: false }]],
-      plugins: [['transform-object-rest-spread', { useBuiltIns: true }]],
-    }),
-    copy({
-      'src/index.js.flow': 'lib/index.js.flow',
-    }),
-  ],
+  external,
+  plugins,
 }];
