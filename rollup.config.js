122eb62a41f9a5b1a589a12358ed4d570a176c11
Jan Grzegorowski <grzegorowski@gmail.com>
2018-10-05 22:50:48 +0200

    chore: improve Rollup building process

--- rollup.config.js
+++ rollup.config.js
@@ -2,40 +2,39 @@ import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-cpy';
 import flow from 'rollup-plugin-flow';
 
-const plugins = [
+const plugins = targets => ([
   flow(),
   babel({
     exclude: 'node_modules/**',
     babelrc: false,
-    presets: [['env', { modules: false }]],
+    presets: [['env', { modules: false, targets }]],
     plugins: [['transform-object-rest-spread', { useBuiltIns: true }]],
+    comments: false,
   }),
   copy({
     files: ['src/*.flow'],
     dest: 'lib',
   }),
-];
+]);
 
 const external = ['axios', 'qs'];
 
 export default [{
   input: 'src/index.js',
   output: {
-    name: 'ES6 Module',
     file: 'lib/index.esm.js',
     format: 'esm',
     sourcemap: true,
   },
   external,
-  plugins,
+  plugins: plugins({ node: '8' }),
 }, {
   input: 'src/index.js',
   output: {
-    name: 'CommonJS',
     file: 'lib/index.js',
     format: 'cjs',
     sourcemap: true,
   },
   external,
-  plugins,
+  plugins: plugins({ node: '6' }),
 }];
